54a9f28338cd7c4ff29b394d2260ba5f6d61d749
HERMANOS <HERMANOS@DESKTOP-10IQNUD>
2024-05-26 20:03:14 -0300

    Se agrega la Estructura del proyecto

---
+++ tp4_DominguezEmanuel/src/main/java/ar/edu/unju/fi/PracticaPaginasWebApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaPaginasWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaPaginasWebApplication.class, args);
+	}
+
+}
