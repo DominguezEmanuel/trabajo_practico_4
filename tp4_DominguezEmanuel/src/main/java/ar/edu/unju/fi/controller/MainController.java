c7a0d4a3c97a15ef8cb9c8709515f0bc98bccf44
HERMANOS <HERMANOS@DESKTOP-10IQNUD>
2024-05-31 23:49:10 -0300

    Se agrega avance de la parte 2 del TP N°4

--- tp4_DominguezEmanuel/src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ tp4_DominguezEmanuel/src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -1,13 +1,17 @@
 package ar.edu.unju.fi.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+//import ch.qos.logback.core.model.Model;
 
 @Controller
 public class MainController {
 
 	@GetMapping("/principal")
-	public String getIndicePage() {
+	public String getIndicePage(Model model) {
+		model.addAttribute(null, model);
 		return "index";
 	}
 }
